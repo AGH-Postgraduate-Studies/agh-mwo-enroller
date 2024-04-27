4d665dcfb92fe0067a8fe76fe0ada4df44b67ea2
Maciej Szostak <maciek.szostak55@gmail.com>
2024-04-27 15:50:15 +0200

    Hash participant password and create `WebSecurity`

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.company.enroller.model.Participant;
@@ -16,6 +17,8 @@ public class ParticipantRestController {
 
 	@Autowired
 	ParticipantService participantService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ResponseEntity<Collection<Participant>> getAll(
@@ -47,6 +50,9 @@ public class ParticipantRestController {
 			return new ResponseEntity<>("Unable to create. A participant with login "
 					+ participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
 		}
+
+		String hashedPassword = passwordEncoder.encode(participant.getPassword());
+		participant.setPassword(hashedPassword);
 
 		participantService.create(participant);
 		return new ResponseEntity<Participant>(participant, HttpStatus.CREATED);
